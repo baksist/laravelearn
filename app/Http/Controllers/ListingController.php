2ff8555828811caf88e29ffccdcc2ff65890f6a4
Matvey Serdyukov <matvey.serdukov@gmail.com>
2024-04-26 16:03:56 +0300

    add search and tag filters

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,9 @@ class ListingController extends Controller
 {
     // show all listings
     public function index(){
+        
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
