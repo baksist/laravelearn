11a7fc2ebdeac77994659c3ad6e153e68dd442f9
Matvey Serdyukov <matvey.serdukov@gmail.com>
2024-11-12 20:13:06 +0300

    add pagination

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -3,15 +3,18 @@
 @include('partials._hero')
 @include('partials._search')
 
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-    @if(count($listings) == 0)
-        <p>No listings found</p>
-    @endif
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @if(count($listings) == 0)
+            <p>No listings found</p>
+        @endif
 
-    @foreach($listings as $listing) {{-- foreach directive --}}
-        {{-- looks like laravel will just ignore this if array is empty --}}
-        <x-listing-card :listing="$listing"/>
-    @endforeach
-</div>
+        @foreach($listings as $listing) {{-- foreach directive --}}
+            {{-- looks like laravel will just ignore this if array is empty --}}
+            <x-listing-card :listing="$listing"/>
+        @endforeach
+    </div>
 
+    <div class="mt-6 p-4">
+        {{$listings->links()}}
+    </div>
 </x-layout>
